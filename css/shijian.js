b34c7bccfdc12eff1fb70ededc8beb749830c32d
xiao015 <82459331+xiao015@users.noreply.github.com>
2022-04-26 22:40:31 +0800

    Update shijian.js

--- css/shijian.js
+++ css/shijian.js
@@ -1,5 +1,5 @@
 function getRTime(){
-var EndTime = new Date('2022/01/01 00:00:00'); //截止时间
+var EndTime = new Date('2023/01/01 00:00:00'); //截止时间
 var NowTime = new Date();
 var t = EndTime.getTime() - NowTime.getTime();
 var d=Math.floor(t/1000/60/60/24);
